df89d10d15603b23bdd3aa4da484ed6cc37b9882
martins <martinmachieb21@gmail.com>
2023-06-10 23:23:26 -1100

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+
+// var video = document.getElementById("myVideo");
+// var btn = document.getElementById("myBtn");
+
+// function myfunction(){
+//   if (video.paused){
+//     video.play();
+//     btn.innerHTML = "pause";
+//   } else {
+//     video.pause();
+//     btn.innerHTML = "play";
+//   }
+// }
+
